595088ba2fe350ddb4c5a92859ea012f13228172
Jonathan Garcia Speçamillio <93947030+KlotJota@users.noreply.github.com>
2023-02-01 15:23:27 -0300

    Add files via upload

---
+++ usando_captcha.py
@@ -0,0 +1,8 @@
+# pegar uma imagem de captcha
+from resolve_captcha import quebrar_captha
+
+texto_captcha = quebrar_captha()
+
+print(f"O texto do CAPTCHA é: {texto_captcha}")
+
+# resolver essa imagem
